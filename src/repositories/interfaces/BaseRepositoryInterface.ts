f1606ec1cc1eda3f393fb4c7c560c1e989707f40
phucchp <phucchp@gmail.com>
2024-01-15 16:48:58 +0700

    fix base repository

--- src/repositories/interfaces/BaseRepositoryInterface.ts
+++ src/repositories/interfaces/BaseRepositoryInterface.ts
@@ -2,10 +2,11 @@ import { Model } from 'sequelize-typescript';
 
 interface BaseRepositoryInterface<T extends Model<T>> {
   getAll(options?: any): Promise<T[]>;
-  getById(id: number, options?: any): Promise<T | null>;
-//   create(entity: Partial<T>): Promise<T>;
-//   update(id: number, entity: Partial<T>): Promise<[number, T[]]>;
+  findById(id: number, options?: any): Promise<T | null>;
+  create(entity: Partial<T>): Promise<T>;
+  update(id: number, entity: Partial<T>): Promise<T | null>;
   delete(id: number): Promise<number>;
+  findOneByCondition(condition: any, options?: any, paranoid?:boolean): Promise<T | null>
 }
 
 export { BaseRepositoryInterface };
