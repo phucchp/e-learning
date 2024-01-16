103defaa220a5405e916d8f27b86ad4f6ba131dc
phucchp <phucchp@gmail.com>
2024-01-16 11:47:42 +0700

    create lesson and topics table

---
+++ src/migrations/20240116025242-create-topics-table.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('topics', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name:{
+        type: Sequelize.STRING(150),
+        allowNull: false,
+      },
+      course_id: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'courses',
+          },
+          key: 'course_id'
+        },
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('topics');
+  }
+};
