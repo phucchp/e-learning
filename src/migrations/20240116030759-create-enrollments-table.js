38eea7c6300274a8c04c30964bca243d9bb7fec4
phucchp <phucchp@gmail.com>
2024-01-16 11:48:20 +0700

    Create comments, favorites, enrollments table

---
+++ src/migrations/20240116030759-create-enrollments-table.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('enrollments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'users',
+          },
+          key: 'id'
+        },
+      },
+      course_id: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'courses',
+          },
+          key: 'course_id'
+        },
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('enrollments');
+  }
+};
