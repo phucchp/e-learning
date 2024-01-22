123ae35b67fa773d3bd237ce4e5f8cad55b6c985
phucchp <phucchp@gmail.com>
2024-01-22 17:29:38 +0700

    Add validate for API register

--- src/routes/AuthenticationRoutes.ts
+++ src/routes/AuthenticationRoutes.ts
@@ -2,6 +2,8 @@ import { Request, Response, NextFunction } from 'express';
 import Authentication from '../utils/Authentication';
 import BaseRoutes from './base/BaseRouter';
 import { AuthenticationController } from '../controllers/AuthenticationController';
+import { validateRegister } from '../validators/UserValidator';
+import { validate } from '../validators/Validate';
 
 class AuthenticationRoutes extends BaseRoutes {
 	constructor() {
@@ -9,7 +11,7 @@ class AuthenticationRoutes extends BaseRoutes {
 	}
 	routes(): void {
 		this.router.post('/login', this.controller.login);
-		this.router.post('/register' ,this.controller.register);
+		this.router.post('/register' ,validateRegister, validate, this.controller.register);
 	}
 }
 
