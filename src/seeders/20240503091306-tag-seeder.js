c83b68c80c9a0806becd1cacc290904d0a11f121
phucchp <phucchp@gmail.com>
2024-05-23 10:45:29 +0700

    Create data for model tags

--- src/seeders/20240503091306-tag-seeder.js
+++ src/seeders/20240503091306-tag-seeder.js
@@ -1,36 +1,47 @@
 'use strict';
 
 const tags = [
-  'android',
-  'applicants',
-  'aws',
-  'business',
-  'coding',
-  'data-science',
-  'design',
-  'development',
-  'html',
-  'css',
-  'node',
-  'programming',
-  'mysql',
-  'seo',
-
+  { tag_id: 'android', name: 'android', created_at: new Date(), updated_at: new Date() },
+  { tag_id: 'applicants', name: 'applicants' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'aws', name: 'aws' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'business', name: 'business' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'coding', name: 'coding' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'data-science', name: 'data-science' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'design', name: 'design' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'development', name: 'development' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'html', name: 'html' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'css', name: 'css' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'css3', name: 'css3' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'node', name: 'node' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'programming', name: 'programming' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'mysql', name: 'mysql' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'seo', name: 'seo' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'ai', name: 'ai' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'bootcamp', name: 'bootcamp' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'bootstrap', name: 'bootstrap' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'firebase', name: 'firebase' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'framework', name: 'framework' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'html5', name: 'html5' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'java', name: 'java' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'javascript', name: 'javascript' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'machine-learning', name: 'machine learning' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'postgresql', name: 'postgresql' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'mongoDB', name: 'mongoDB' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'nodejs', name: 'nodejs' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'php', name: 'php' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'redux', name: 'redux' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'unity', name: 'unity' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'web-applications', name: 'web applications' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'web-apps', name: 'web apps' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'website', name: 'website' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'developer', name: 'developer' , created_at: new Date(), updated_at: new Date()},
+  { tag_id: 'wordpress', name: 'wordpress' , created_at: new Date(), updated_at: new Date()},
 ];
 
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
     await queryInterface.bulkInsert('tags', tags, {});
   },
 
